c0b883912d53e7f3026e71901be43940ca07c8c1
Yue Zheng <zheng.yue1@husky.neu.edu>
2020-03-23 22:48:48 -0400

    add recommendation

--- src/main/java/rpc/RecommendItem.java
+++ src/main/java/rpc/RecommendItem.java
@@ -1,25 +1,41 @@
 package rpc;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import entity.Item;
 import org.json.JSONArray;
-import org.json.JSONObject;
+import recommendation.Recommendation;
 
 public class RecommendItem extends HttpServlet {
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		JSONArray array = new JSONArray();
-		array.put(new JSONObject().put("name", "abcd").put("address", "San Francisco").put("time", "01/01/2017"));
-		array.put(new JSONObject().put("name", "1234").put("address", "San Jose").put("time", "01/01/2017"));
-		RpcHelper.writeJsonArray(response, array);
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            response.setStatus(403);
+            return;
+        }
+        String userId = request.getParameter("user_id");
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
+        double lat = Double.parseDouble(request.getParameter("lat"));
+        double lon = Double.parseDouble(request.getParameter("lon"));
+
+        Recommendation recommendation = new Recommendation();
+        List<Item> items = recommendation.recommendItems(userId, lat, lon);
+        JSONArray array = new JSONArray();
+        for (Item item : items) {
+            array.put(item.toJSONObject());
+        }
+        RpcHelper.writeJsonArray(response, array);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
 }
